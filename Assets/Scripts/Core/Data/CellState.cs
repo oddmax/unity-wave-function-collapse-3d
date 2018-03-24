cdc8d07ff8f7131e04c9274ecbbabf4c2097e620
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-03-24 23:46:06 +0100

    add CellState class to have intermediate info about cell

---
+++ Assets/Scripts/Core/Data/CellState.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Data
+{
+    public class CellState
+    {
+        public float EntropyLevel;
+        
+        /// <summary>
+        /// Going to be null if Entropy level is more than 0
+        /// </summary>
+        public byte? TileIndex;
+
+        public CellState(float entropyLevel, byte? tileIndex)
+        {
+            EntropyLevel = entropyLevel;
+            TileIndex = tileIndex;
+        }
+    }
+}
