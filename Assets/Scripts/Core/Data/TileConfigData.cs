17628997f7dbd044e7ab5cc1d8abbc51dba1e893
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-04-02 23:12:11 +0200

    add TileConfig data class

---
+++ Assets/Scripts/Core/Data/TileConfigData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Data
+{
+    public class TileConfigData<T> where T : TileConfig
+    {
+        private readonly Dictionary<string, T> Configs = new Dictionary<string, T>();
+
+        public T GetConfig(string configId)
+        {
+            if (Configs.ContainsKey(configId))
+            {
+                return Configs[configId];
+            }
+
+            return null;
+        }
+
+        public void AddConfig(T config)
+        {
+            Configs.Add(config.Id, config);
+        }
+    }
+}
