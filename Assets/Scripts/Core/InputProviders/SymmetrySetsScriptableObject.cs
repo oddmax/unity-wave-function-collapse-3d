f7038d3a92623ef3557b4dc0e670909419b5856b
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-04-02 23:11:57 +0200

    add Symmetry sets SO

---
+++ Assets/Scripts/Core/InputProviders/SymmetrySetsScriptableObject.cs
@@ -0,0 +1,55 @@
+﻿using Core.Data.SimpleTiledModel;
+using Core.Helpers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Core.InputProviders
+{
+    public class SymmetrySetsScriptableObject : ScriptableObject
+    {
+        [SerializeField] 
+        public GameObject[] SymmetryX;
+        
+        [SerializeField] 
+        public GameObject[] SymmetryL;
+        
+        [SerializeField] 
+        public GameObject[] SymmetryT;
+        
+        [SerializeField] 
+        public GameObject[] SymmetryI;
+        
+        [SerializeField] 
+        public GameObject[] SymmetrySlash;
+        
+        [MenuItem("Assets/Create/SymmetrySetsScriptableObject")]
+        public static void CreateAsset ()
+        {
+            ScriptableObjectUtility.CreateAsset<SymmetrySetsScriptableObject>();
+        }
+
+        public SymmetryType GetSymmetryByTileName(string name)
+        {
+            if (Find(SymmetryX, name)) return SymmetryType.X;
+            if (Find(SymmetryL, name)) return SymmetryType.L;
+            if (Find(SymmetryT, name)) return SymmetryType.T;
+            if (Find(SymmetryI, name)) return SymmetryType.I;
+            if (Find(SymmetrySlash, name)) return SymmetryType.Slash;
+
+            return SymmetryType.X;
+        }
+
+        private bool Find(GameObject[] set, string name)
+        {
+            foreach (var gameObject in set)
+            {
+                if (gameObject.name == name)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
