06cbd901cafc64b50b6ab69a9c1a338bc0cfbaca
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-04-04 23:08:21 +0200

    enhance tile config data

--- Assets/Scripts/Core/Data/TileConfigData.cs
+++ Assets/Scripts/Core/Data/TileConfigData.cs
@@ -4,13 +4,20 @@ namespace Core.Data
 {
     public class TileConfigData<T> where T : TileConfig
     {
-        private readonly Dictionary<string, T> Configs = new Dictionary<string, T>();
+        private readonly Dictionary<string, T> ConfigsMap = new Dictionary<string, T>();
+
+        public TileConfigData()
+        {
+            ConfigsList = new List<T>();
+        }
+
+        public List<T> ConfigsList { get; private set; }
 
         public T GetConfig(string configId)
         {
-            if (Configs.ContainsKey(configId))
+            if (ConfigsMap.ContainsKey(configId))
             {
-                return Configs[configId];
+                return ConfigsMap[configId];
             }
 
             return null;
@@ -18,7 +25,8 @@ namespace Core.Data
 
         public void AddConfig(T config)
         {
-            Configs.Add(config.Id, config);
+            ConfigsMap.Add(config.Id, config);
+            ConfigsList.Add(config);
         }
     }
 }
