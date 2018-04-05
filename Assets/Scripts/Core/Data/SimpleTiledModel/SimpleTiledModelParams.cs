6a24908eb224136011e3065aa6724dda56e8adb5
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-04-05 21:45:58 +0200

    add comments

--- Assets/Scripts/Core/Data/SimpleTiledModel/SimpleTiledModelParams.cs
+++ Assets/Scripts/Core/Data/SimpleTiledModel/SimpleTiledModelParams.cs
@@ -2,9 +2,24 @@
 {
     public class SimpleTiledModelParams : WaveFunctionCollapseModelParams
     {
+        public const string DEFAULT_SUBSET = "Default";
+       
+        /// <summary>
+        /// Determines if the output solutions are tilable. It's usefull for creating things like tileable textures,
+        /// but also has a surprising influence on the output. When working with WFC, it's often a good idea to toggle
+        /// Periodic Output on and off, checking if either setting influences the results in a favorable way.
+        /// </summary>
+        public bool Periodic { get; private set; }
         
-        public SimpleTiledModelParams(int width, int depth) : base(width, depth)
+        /// <summary>
+        /// Defines which subset of tiles to use from Input data
+        /// </summary>
+        public string SubsetName { get; private set; }
+        
+        public SimpleTiledModelParams(int width, int depth, bool periodic = false, string subsetName = DEFAULT_SUBSET) : base(width, depth)
         {
+            Periodic = periodic;
+            SubsetName = subsetName;
         }
     }
 }
