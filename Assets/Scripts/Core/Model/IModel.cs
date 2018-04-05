a95dca91c73abe65b8ad9bba6292330d43c1113a
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-04-05 23:45:45 +0200

    introduce model interface

---
+++ Assets/Scripts/Core/Model/IModel.cs
@@ -0,0 +1,11 @@
+﻿using Core.Data;
+
+namespace Core.Model
+{
+    public interface IModel
+    {
+        CellState GetCellStateAt(int x, int y);
+
+        WaveFunctionCollapseModelParams ModelParam { get; }
+    }
+}
