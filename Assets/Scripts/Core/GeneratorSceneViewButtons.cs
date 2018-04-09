fc924f6fa051c9e225ae8ce79316b738ba5f49ba
Maxim Priakhin <maxim.priakhin@innogames.com>
2018-04-09 22:29:04 +0200

    scene buttons to simplify work

---
+++ Assets/Scripts/Core/GeneratorSceneViewButtons.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Core
+{
+    [InitializeOnLoad]
+    public static class GeneratorSceneViewButtons {
+
+        static GeneratorSceneViewButtons()
+        {
+            SceneView.onSceneGUIDelegate -= OnSceneGUI;
+            SceneView.onSceneGUIDelegate += OnSceneGUI;
+        }
+
+        static void OnSceneGUI(SceneView sceneView)
+        {
+            var generator = Object.FindObjectOfType<WaveFunctionCollapseGenerator>();
+            if (generator == null)
+            {
+                return;
+            }
+		
+            Handles.BeginGUI();
+		
+            GUILayout.BeginArea(new Rect(sceneView.position.width - 190, sceneView.position.height - 110, 180, 100));
+		
+            if(GUILayout.Button("Generate Overlapping output")){
+                generator.GenerateOverlappingOutput();
+            }
+            if(GUILayout.Button("Generate Simple tiled output")){
+                generator.GenerateSimpleTiledOutput();
+            }
+            if(GUILayout.Button("Abort and Clear")){
+                generator.Abort();
+            }
+		
+            GUILayout.EndArea();
+		
+            Handles.EndGUI();
+        }
+    }
+}
